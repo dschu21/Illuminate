63f682fe77635479effc68ad44e974328c271abc
Danyon <danyonchu.21@gmail.com>
2015-10-05 18:53:10 -0400

    initial commit

---
+++ app/src/main/java/ledcontrol/androidarduinocollab/SplashScreen.java
@@ -0,0 +1,70 @@
+package ledcontrol.androidarduinocollab;
+
+/**
+ * Created by Danyon on 2015-10-05.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_splash_screen);
+
+// METHOD 1
+
+        /****** Create Thread that will sleep for 5 seconds *************/
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 3 seconds
+                    sleep(3*1000);
+
+                    // After 3 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),Main_Screen.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+//METHOD 2
+
+        /*
+        new Handler().postDelayed(new Runnable() {
+
+            // Using handler with postDelayed called runnable run method
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainSplashScreen.this, FirstScreen.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 5*1000); // wait for 5 seconds
+        */
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+    }
+}
